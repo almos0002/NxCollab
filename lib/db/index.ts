40294526c2e7b6a7a870a258dc41ea250d11e2bf
almos0002 <almos0002@gmail.com>
2026-06-17 08:21:24 +0545

    fix the import time issue

--- lib/db/index.ts
+++ lib/db/index.ts
@@ -6,12 +6,9 @@ const { Pool } = pg;
 
 const connectionString = process.env.DATABASE_URL;
 
-if (!connectionString) {
-  throw new Error("DATABASE_URL must be set.");
-}
 
 export const pool = new Pool({
-  connectionString,
+  connectionString: connectionString ?? undefined,
 });
 export const db = drizzle(pool, { schema });
 
